4cf297ead0b856b2de06376da4b6148829dfff42
Haoting Qin <1165101405@qq.com>
2023-12-24 06:04:50 +0800

    暂时关闭登录验证码检测，方便后端接口测试

--- campus-framework/src/main/java/com/oddfar/campus/framework/web/service/SysLoginService.java
+++ campus-framework/src/main/java/com/oddfar/campus/framework/web/service/SysLoginService.java
@@ -60,9 +60,10 @@ public class SysLoginService {
     public String login(String username, String password, String code, String uuid) {
         boolean captchaEnabled = configService.selectCaptchaEnabled();
         // 验证码开关
-        if (captchaEnabled) {
-            validateCaptcha(username, code, uuid);
-        }
+        //TODO 关了验证码
+//        if (captchaEnabled) {
+//            validateCaptcha(username, code, uuid);
+//        }
         // 用户验证
         Authentication authentication = null;
         try {
